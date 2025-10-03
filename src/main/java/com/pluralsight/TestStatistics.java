dd426fe51201259dfb5248d071f13f4ea17285ef
keisha De La Rosa <keishdlr@gmail.com>
2025-10-03 15:19:50 -0400

    created array

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -5,6 +5,8 @@ public class TestStatistics {
     public static void main(String[] args) {
 
         //Create an array of 10 test scores
+        int[] Scores = {60, 80, 78, 69, 92, 87, 71, 91, 66, 75};
 
+        // Print out average, high score, low score
     }
 }
