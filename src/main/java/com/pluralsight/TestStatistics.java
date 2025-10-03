7154f76910dea54d4f8878a51d4f602c6bcefe14
keisha De La Rosa <keishdlr@gmail.com>
2025-10-03 15:34:24 -0400

    For loops for average, max, and min

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -8,5 +8,27 @@ public class TestStatistics {
         int[] Scores = {60, 80, 78, 69, 92, 87, 71, 91, 66, 75};
 
         // Print out average, high score, low score
+
+        double avgScore = 0;
+        int scoreSum = 0;
+
+        for (int i = 0; i < Scores.length; i++) {
+            scoreSum += Scores[i];
+        }
+
+        avgScore = scoreSum / Scores.length;
+        System.out.println("The average is: " + avgScore);
+
+        int max = Scores[0];
+        for ( int i = 0; i < Scores.length; i ++) {
+            max = Scores[i];
+        }
+        System.out.println("The highest Score is: " + max);
+
+        int min = Scores[0];
+        for (int i = 0; i < Scores.length; i++) {
+            min = Scores[i];
+        }
+        System.out.println("The lowest Score is: " + min);
     }
 }
